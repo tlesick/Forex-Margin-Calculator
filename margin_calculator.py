9291a17f83bd0153145dbde4655d6f202c9b8e59
Tim <tlesick@gmail.com>
2018-08-28 12:18:04 -0700

    USD based currencies working only

--- margin_calculator.py
+++ margin_calculator.py
@@ -1,48 +1,58 @@
+import re
+from collections import OrderedDict
+
 class Margin_Calculator(object):
     # EX: 1.15000 sell eurusd 25.00 50:1 194,779 
     def calculate(self, **kwargs):
         # Organize Key word arguments
-        entry, direction, pair, lot_size, capitial, risk_of_trade, margin, stop_loss = 0,0,0,0,0,0,0,0
-        for key, value in kwargs.items():
+        kwargs = OrderedDict(kwargs)
+        entry, direction, pair, lot_size, capital, risk_of_trade, margin, stop_loss = 0,0,0,0,0,0,0,0
+        for key, value in kwargs.items():            
             if key == 'entry':
                 entry = value
                 continue
-            else:
+            elif entry == 0 and list(kwargs.keys())[-1]:
                 raise ValueError('Entry Position is required: e.g. 1.15000')
+            
             if key == 'direction':
                 direction = value
-                print('direction '+str(direction))
                 continue
-            else:
+            elif direction == 0 and list(kwargs.keys())[-1]:
                 raise ValueError('Direction is required: e.g. "buy","Sell","Long","short"')
+            
             if key == 'pair':
-                pair = value
-                print('pair ' + str(pair))
-                continue
-            else:
+                if 'USD' in value:
+                    pair = value
+                    continue
+                else:
+                    raise ValueError('USD only pairs at this time')
+            elif pair == 0 and list(kwargs.keys())[-1]:
                 raise ValueError('pair is required: e.g. "EUR/USD" "USD/JPY"')
+            
             if key == 'lot_size':
                 lot_size = value
                 continue
-            else: 
+            elif lot_size == 0 and list(kwargs.keys())[-1]:
                 raise ValueError("lot_size is required: e.g. 1.0 or 0.05")
-            if key == 'capitial':
-                capitial = value
+            
+            if key == 'capital':
+                capital = value
                 continue
-            else:
-                raise ValueError("Capitial is required: e.g 5000")
+            elif capital == 0 and list(kwargs.keys())[-1]:
+                raise ValueError("capital is required: e.g 5000")
+            
             if key == 'risk':
                 risk_of_trade = value
                 continue
-            else:
+            elif risk_of_trade == 0 and kwargs.keys()[-1]:
                 raise ValueError("Risk is required (percent in decimal form) e.g. 0.02")
+            
             if key == 'margin':
                 margin = value
                 continue
-
-
-            
+    
         # Static Margin Requirements from OANDA AUG 2018 
+        # need to look at the pair that is being traded if the USD is not in the second place then the counter currency will be the measure of the pip
         margin_of_pairs = {'AUD/CAD':3.0,'AUD/CHF':3.0,'AUD/HKD':5.0,'AUD/JPY':4.0,'AUD/NZD':3.0,'AUD/SGD':5.0,'AUD/USD':3.0,'CAD/CHF':3.0,'CAD/HKD':5.0,'CAD/JPY':4.0,
         'CAD/SGD':5.0,'CHF/HKD':5.0,'CHF/JPY':4.0,'CHF/ZAR':5.0,'EUR/AUD':3.0,'EUR/CAD':2.0,'EUR/CHF':3.0,'EUR/CZK':5.0,'EUR/DKK':2.0,'EUR/GBP':5.0,'EUR/HKD':5.0,
         'EUR/HUF':5.0,'EUR/JPY':4.0,'EUR/NOK':3.0,'EUR/NZD':3.0,'EUR/PLN':5.0,'EUR/SEK':3.0,'EUR/SGD':5.0,'EUR/TRY':5.0,'EUR/USD':2.0,'EUR/ZAR':5.0,'GBP/AUD':5.0,
@@ -53,46 +63,81 @@ class Margin_Calculator(object):
 
         # List of Pairs that are not to the 5th decimal place (extremely weak)
         different_decimal_pairs = ['JPY','ZAR','SEK','HUF']
-        
-        # Convert Margin into percentage 
-        if margin == 0: 
-            for name, percent in margin_of_pairs.items():
-                if pair == name:
-                    margin = (percent /100)
-        else:
-            margin = (margin/100)
-            
-
-        # WEAK CURRENCY PAIRS
+        decimal_change = False
         for i in different_decimal_pairs:
             if i in pair:
-                cost_of_margin = margin * (lot_size*100000)
-                remaining_capital = capitial - cost_of_margin
-                loss_risk = remaining_capital  * risk_of_trade
-                number_of_pips = loss_risk/ lot_size
-                if direction == 'sell' or direction == 'Sell' or direction='short' or direction='Short':
-                    stop_loss = (number_of_pips/1000) + entry 
-                if direction == 'Buy' or direction == 'buy' or direction='long' or direction='Long':
-                    stop_loss = entry - (number_of_pips/1000)  
-                return stop_loss
-        # NORMAL PAIRS
+                decimal_change = True
+                break
+
+
+        # 100,000*0.01 (the 2nd decimal) /99.735≈$10.03 how to determine the pip price
+        # if the margin is in another currency then you have to convert back to that currency
+
+        # Determine the Counter Currency
+        for name, percent in margin_of_pairs.items():
+            decimal_check = re.compile('^\d+\.\d{0,2}$')
+            # Make sure that the pair exists
+            if str(name) == str(pair):
+
+                # Determine Margin
+                if margin == 0:
+                    # use margin dict
+                    margin = (percent/100)
+                    break
+                    # convert margin
+                elif decimal_check.match(margin): 
+                    margin = (margin/100)
+                    break
+                else:
+                    raise ValueError('Margin in wrong format e.g. 10% = 10.0')
+            # Remove the pair does not exist and no margin is given
+            if name != pair and name == 'ZAR/JPY':
+                raise ValueError("Pair does not match. e.g. EUR/USD")
+            
+        # Determine the total amount willing to risk
+        cost_of_margin = margin * (lot_size*100000)
+        remaining_capital = capital - cost_of_margin
+        loss_risk = remaining_capital  * risk_of_trade
+        # Dollar is base currency
+        if '/USD' in pair:
+            number_of_pips = loss_risk/ lot_size
+
+        # Counter Currency where the dollar is not the base currency
+        if 'USD/' in pair:
+            # find the location of the decimal
+            decimal = str(entry).find('.')
+            # get the number of decimals
+            number_of_decimals = len(str(entry)[decimal+1:])
+            # only two cases I know currently
+            if number_of_decimals == 5:
+                multipler = 0.0001
+            if number_of_decimals == 3:
+                multipler = 0.01
+            # add the number of 0's before the one
+            movement_per_pip = (10000*multipler)/entry
+            number_of_pips = int(loss_risk/movement_per_pip)
+
+        
+        # Determine the movement up or down
+        if direction == 'sell' or direction == 'Sell' or direction == 'short' or direction == 'Short':
+            if decimal_change:
+                stop_loss = (number_of_pips/1000) + entry 
             else:
-                cost_of_margin = margin * (lot_size*100000)
-                remaining_capital = capitial - cost_of_margin
-                loss_risk = remaining_capital  * risk_of_trade
-                print('Total amount possibly lost: ' + str(loss_risk))
-                number_of_pips = loss_risk/ lot_size
-                if direction == 'sell' or direction == 'Sell' or direction='short' or direction='Short':
-                    stop_loss = (number_of_pips/100000) + entry 
-                if direction == 'Buy' or direction == 'buy' or direction='long' or direction='Long':
-                    stop_loss =  entry - (number_of_pips/100000)   
-                return stop_loss
+                stop_loss = (number_of_pips/100000) + entry 
+        if direction == 'Buy' or direction == 'buy' or direction =='long' or direction =='Long':
+            if decimal_change:
+                stop_loss =  entry - (number_of_pips/1000)
+            else:
+                stop_loss =  entry - (number_of_pips/100000)   
+        close_out_capital = capital - loss_risk
+        return {'stop_loss': stop_loss,
+                'potential_loss': loss_risk,
+                'close_out_capital': close_out_capital}
 
-        
 
-        
-        # TEST
 
-# print(Margin_Calculator().calculate(entry=111.067, direction='Sell',pair='USD/JPY',lot_size=1.00, capitial=5000, risk=0.02))
-# print(Margin_Calculator().calculate( direction='Sell',pair='EUR/USD',lot_size=1.00, capitial=5000))
-# print(Margin_Calculator().calculate(entry=1.15000, direction='Sell',pair='EUR/USD',lot_size=0.05, capitial=5000, risk=0.02))
+
+# Works in current configuration   
+print(Margin_Calculator().calculate(entry=111.067, direction='Sell',pair='USD/JPY',lot_size=1.00, capital=5000, risk=0.02))
+
+print(Margin_Calculator().calculate(entry=1.15000, direction='Sell',pair='EUR/USD',lot_size=0.05, capital=5000, risk=0.02))
